442ee8ed3cbe27ba9c42a38f3c26801cecaeed92
Chad Dalrymple <chaddalrymple@macbookpro.lan>
2025-02-21 14:15:19 -0500

    hopefully fix routing

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000/todos"
+const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/todos"
 
 export const getTodos = async () => {
   const response = await fetch(API_URL);
