58ce439d477b5a0133358d27a16ae96bde1eb5ff
Chad Dalrymple <chaddalrymple@macbookpro.lan>
2025-02-21 11:04:06 -0500

    add runtime

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from datetime import datetime
-from os import os
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend
