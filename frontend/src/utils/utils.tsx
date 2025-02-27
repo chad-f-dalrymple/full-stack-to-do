f2d88663c1ef617246aa89c6f56b515149a5bfb1
Chad Dalrymple <chaddalrymple@macbookpro.lan>
2025-02-26 22:54:35 -0500

    updating package json

--- frontend/src/utils/utils.tsx
+++ frontend/src/utils/utils.tsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api/todos"
+const API_URL = "/api/todos";
 
 export const getTodos = async () => {
   const response = await fetch(API_URL);
